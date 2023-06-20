465cf99500eaf32a418166ca17b4aa8f4fdd7d33
FRAALES <alessandrofranco1992@gmail.com>
2023-06-20 17:29:50 +0200

    tabella fatta ma le td e le tr sono invertite

--- index.php
+++ index.php
@@ -40,6 +40,84 @@ $hotels = [
 
 ];
 
-echo "<pre>";
-var_dump ($hotels);
-echo "</ pre";
+// echo "<pre>";
+// var_dump ($hotels);
+// echo "</ pre";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    
+    <style>
+    
+    </style>
+    </head>
+    <body>
+        <!-- Qui inserisco la tabella con i 5 hotel e i loro dati -->
+
+        <table class="table table-info">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome Hotel</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <?php
+                    foreach ($hotels as $hotel) {
+                        echo '<td>' . $hotel['name'] . '</td>';
+                    }
+                    ?>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <?php
+                    foreach ($hotels as $hotel) {
+                        echo '<td>' . $hotel['description'] . '</td>';
+                    }
+                    ?>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <?php
+                    foreach ($hotels as $hotel) {
+                        echo '<td>' . $hotel['parking'] . '</td>';
+                    }
+                    ?>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <?php
+                    foreach ($hotels as $hotel) {
+                        echo '<td>' . $hotel['vote'] . '</td>';
+                    }
+                    ?>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <?php
+                    foreach ($hotels as $hotel) {
+                        echo '<td>' . $hotel['distance_to_center'] . '</td>';
+                    }
+                    ?>
+                </tr>
+            </tbody>
+        </table>
+            
+    </body>
+</html>
